a24b7262fc228daf23d4fd26834216801c7499be
Laurent Mignot <laurent@mignot.eu>
2015-10-18 23:39:15 +0100

    pi

--- day-01/ex18.groovy
+++ day-01/ex18.groovy
@@ -1,6 +1,22 @@
 // 18 Pi
-//
+// 628 iterations to get 3.14 correct
 
-println "Enter a number:";
+println "Please enter the number of iterations you want to calculate:";
 int input = Integer.parseInt(System.console().readLine());
 
+
+int i = 1;
+double calc = 0;
+
+long startTime = System.nanoTime();
+
+while (i < input) {
+    calc = calc + ((1 / (2 * i - 1)) - (1 / (2 * i + 1)));
+    i += 2;
+}
+
+double pi = 4 * calc;
+long endTime = System.nanoTime();
+
+println "calculated " + input + " iterations in ~" + (int) ((endTime - startTime) / 1000000) % 60 + "ms";
+println pi;
