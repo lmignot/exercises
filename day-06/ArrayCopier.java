475f5b033d76953e877c19476c7d7e0426363bf6
Laurent Mignot <laurent@mignot.eu>
2015-11-02 20:46:28 +0000

    moved print method to array copier class

--- day-06/ArrayCopier.java
+++ day-06/ArrayCopier.java
@@ -11,4 +11,16 @@ public class ArrayCopier {
         }
     }
 
+    public void printArray (int[] arr) {
+        System.out.print(" values: ");
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i < arr.length - 1) {
+                System.out.print(arr[i] + ", ");
+            } else {
+                System.out.println(arr[i]);
+            }
+        }
+    }
+
 }
