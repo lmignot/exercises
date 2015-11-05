b0f8ca49651770f8adfbf9b6ae9d034a0812b2a9
Laurent Mignot <laurent@mignot.eu>
2015-11-05 22:47:43 +0000

    initial commit, matrix checker

---
+++ day-06/MatrixChecker.java
@@ -0,0 +1,15 @@
+public class MatrixChecker {
+
+    public boolean isSymmetrical (int[] ints) {
+
+    }
+
+    public boolean isSymmetrical (int[][] matrix) {
+
+    }
+
+    public boolean isTriangular (int[][] matrix) {
+
+    }
+
+}
