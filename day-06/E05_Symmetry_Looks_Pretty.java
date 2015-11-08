373170910e88ee0536b92a5039e79a707ca5c4f4
Laurent Mignot <laurent@mignot.eu>
2015-11-08 16:44:05 +0000

    matrix symmetry

--- day-06/E05_Symmetry_Looks_Pretty.java
+++ day-06/E05_Symmetry_Looks_Pretty.java
@@ -9,5 +9,10 @@ public class E05_Symmetry_Looks_Pretty {
 
         System.out.println("symmetrical check ({1,3,5,7,7,5,3,1}): " + matrixChecker.isSymmetrical(symmetricalArr));
         System.out.println("asymmetrical check ({2,5,7,3,2,6,3,6}): " + !matrixChecker.isSymmetrical(asymmetricalArr));
+
+        int[][] matrix = {{1,9,8},{9,7,6},{8,6,5}};
+
+        System.out.println("a matrix ({{1,9,8},{9,7,6},{8,6,5}}) is symmetrical: " + matrixChecker.isSymmetrical
+                (matrix));
     }
 }
--- day-06/MatrixChecker.java
+++ day-06/MatrixChecker.java
@@ -16,6 +16,18 @@ public class MatrixChecker {
     }
 
     public boolean isSymmetrical (int[][] matrix) {
+        // given the rule from the exercise:
+        // An matrix is symmetrical if m[i][j] == m[j][i] for any value of i and j.
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                System.out.println(matrix[row][col]);
+                System.out.println(matrix[col][row]);
+                if(matrix[row][col] != matrix[col][row]) {
+                    return false;
+                }
+            }
+        }
+
         return true;
     }
 
