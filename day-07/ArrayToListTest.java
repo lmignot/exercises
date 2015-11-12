1587d3dd012d71159f665bdb43e7bc23668afa88
Laurent Mignot <laurent@mignot.eu>
2015-11-12 22:30:01 +0000

    Test for exercise 9

---
+++ day-07/ArrayToListTest.java
@@ -0,0 +1,14 @@
+public class ArrayToListTest {
+
+    public static void main(String[] args) {
+        ListUtilities utils = new ListUtilities();
+
+        IntegerList intList = utils.arrayToIntegerList(new int[] {1,5,6,8,2,98,23,7,4,98,21,93,84,17,109}, false);
+
+        System.out.println("Integer list initialised, size is: " + intList.getSize());
+        System.out.println("");
+        intList.prettyPrint();
+
+    }
+
+}
