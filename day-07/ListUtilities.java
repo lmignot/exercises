3e2f305319e6f7a67646de5b44a888874c37f36a
Laurent Mignot <laurent@mignot.eu>
2015-11-29 18:07:51 +0000

    improved bubbleSort... not using the getAt() method as that performs an additional loop so was inefficient

--- day-07/ListUtilities.java
+++ day-07/ListUtilities.java
@@ -17,27 +17,27 @@ public class ListUtilities {
         } else if (size == 1) {
             System.out.println("Only one node in list, already sorted.");
         } else {
-            IntegerNode current;
+            IntegerNode current = list.getStart();
 
             // keep track of whether we did a swap in the inner loop
             // if we did not swap for a full iteration
             // we can stop the outer loop
             boolean didSwap = false;
 
-            for (int i = 0; i < size; i++) {
+            while (current.getNext() != null) {
+                IntegerNode inner = list.getStart();
                 didSwap = false;
-                for (int j = 0; j < size; j++) {
-                    current = list.getAt(j);
-                    if (current.getNext() != null &&
-                        current.getValue() > current.getNext().getValue()) {
-                        // System.out.println("**********");
-                        // System.out.println("swapping: " + current.getValue() + " <> " + current.getNext().getValue());
-                        // System.out.println("**********");
-                        list.swap(current, current.getNext());
+
+                while (inner.getNext() != null) {
+                    if (inner.getNext() != null &&
+                        inner.getValue() > inner.getNext().getValue()) {
+                        list.swap(inner, inner.getNext());
                         didSwap = true;
                     }
-                    // list.prettyPrint();
+                    inner = inner.getNext();
                 }
+
+                current = current.getNext();
                 if (!didSwap) {
                     break;
                 }
@@ -47,13 +47,13 @@ public class ListUtilities {
         return list;
     }
 
-    public static IntegerList cocktailSort(IntegerList list) {
-        // TODO: implement bubbleSort
+    public static IntegerList cocktailSort (IntegerList list) {
+        // TODO: implement cocktailSort
         return list;
     }
 
-    public static IntegerList quickSort(IntegerList list) {
-        // TODO: implement bubbleSort
+    public static IntegerList quickSort (IntegerList list) {
+        // TODO: implement quickSort
         return list;
     }
 
