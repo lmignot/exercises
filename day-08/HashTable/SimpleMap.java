c4caff31567fd80d88b2aba2858da1cab1124820
Laurent Mignot <laurent@mignot.eu>
2015-12-06 21:08:42 +0000

    initial files for HashTable

---
+++ day-08/HashTable/SimpleMap.java
@@ -0,0 +1,25 @@
+/**
+ * Map from integer to Strings: one to many
+ */
+public interface SimpleMap {
+    /**
+     * Puts a new String in the map.
+     */
+    String put (int key, String name);
+
+    /**
+     * Returns all names associated with the key
+     * or null if there is none.
+     */
+    String[] get (int key);
+
+    /**
+     * Removes a name from the map
+     */
+    void remove (int key, String name);
+
+    /**
+     * @return true if no entries in map, false otherwise
+     */
+    boolean isEmpty ();
+}
