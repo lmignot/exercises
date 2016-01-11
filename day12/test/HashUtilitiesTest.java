a1edf2f42713440c5d025df2567ea734a66f7e8a
Laurent Mignot <laurent@mignot.eu>
2016-01-11 19:43:55 +0000

    import without using wildcard

--- day12/test/HashUtilitiesTest.java
+++ day12/test/HashUtilitiesTest.java
@@ -1,6 +1,7 @@
 import java.util.Random;
-import org.junit.*;
-import static org.junit.Assert.*;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class HashUtilitiesTest {
 
