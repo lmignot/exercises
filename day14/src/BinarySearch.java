ba33fec1535c1f8f7eff60f675cd758f16ebe10c
Laurent Mignot <laurent@mignot.eu>
2016-01-25 20:40:40 +0000

    day 14 - exercise 4 - Binary Search - incomplete

---
+++ day14/src/BinarySearch.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+/**
+ * Binary search using recursion
+ */
+public class BinarySearch {
+
+    private boolean search(List<Integer> l, Integer query) {
+        if (l == null) {
+            return false;
+        } else if (l.isEmpty()) {
+            return false;
+        } else {
+
+            return true;
+        }
+    }
+
+    public static void main (String[] args) {
+
+    }
+}
