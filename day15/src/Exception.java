4346046c1c174d7799302403282920e30bc60b7d
Laurent Mignot <laurent@mignot.eu>
2016-02-01 18:42:45 +0000

    day 15 - exercise 2 - answer in comment

--- day15/src/Exception.java
+++ day15/src/Exception.java
@@ -1,6 +1,9 @@
 /**
  * Day 15 - exercise 2 - Exception
+ * The less specific Exception will be caught so it will
+ * never reach the more specific exception below it
+ * Exception handling code should catch more specific exceptions first
  */
 public class Exception {
-    
+
 }
