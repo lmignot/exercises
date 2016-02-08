a0429ee2b45485daee3d79e5ceb87c101f13aa34
Laurent Mignot <laurent@mignot.eu>
2016-02-08 22:17:34 +0000

    day 16 exercise 4b - add ability to read a list of files and a destination folder

--- day16/src/FileCp.java
+++ day16/src/FileCp.java
@@ -14,16 +14,40 @@ public class FileCp {
         String original;
         String destination;
 
-        if (args.length == 0) {
-            System.out.println("Please specify a source and destination");
-        } else if (args.length == 1) {
-            System.out.println("Please provide the name for the copy");
-        } else if (args.length == 2) {
-            original = args[0];
-            destination = args[1];
+        switch (args.length) {
+            case 0:
+                System.out.println("Please specify a source and destination, or a list of files and destination " +
+                        "folder");
+                break;
+            case 1: {
+                System.out.println("Please specify source file(s) and destination filename or folder");
+                break;
+            }
+            case 2: {
+                original = args[0];
+                destination = args[1];
 
-            if (cp(original, destination)) {
-                System.out.println("Copy successful!");
+                if (cp(original, destination)) {
+                    System.out.println("Copy successful!");
+                }
+                break;
+            }
+            default: {
+                String destPath = args[args.length - 1];
+                File dest = new File(CWD + File.separator + destPath);
+                if(!dest.isDirectory()) {
+                    System.out.println("Last argument should be a destination folder.");
+                } else {
+                    for (int i = 0; i < args.length - 1; i++) {
+                        if (cp(args[i], dest.getName() + File.separator + args[i])) {
+                            System.out.println("Copied " + args[i] + " to " +
+                                    dest.getName() + File.separator + args[i]);
+                        } else {
+                            System.out.println("Failed to copy " + args[i]);
+                        }
+                    }
+                }
+                break;
             }
         }
     }
@@ -31,12 +55,14 @@ public class FileCp {
     public static boolean cp (String original, String destination) {
         File origin = new File(CWD + File.separator + original);
         File dest = new File(CWD + File.separator + destination);
-        String confirmation = null;
+        String confirmation;
 
         if (!origin.exists()) {
             System.err.println("File " + original + " does not exist, please try again.");
             return false;
         }
+
+        // destination file exists so get an overwrite confirmation
         if (dest.exists()) {
             System.out.print("A file named " + destination + " exists, do you want to overwrite it? (Y/N) ");
             Scanner sc = new Scanner(System.in);
@@ -45,11 +71,17 @@ public class FileCp {
                 System.out.println("Exiting... goodbye!");
                 return false;
             }
+        } else {
+            confirmation = "y";
         }
 
         if (confirmation != null) {
             System.out.println("Copying " + origin.getPath() + " to " + dest.getPath());
+
+            // read in the file contents
             try (BufferedReader in = new BufferedReader(new FileReader(origin))) {
+                
+                // write the file contents to the destination file
                 try (Writer writer = new BufferedWriter(
                     new OutputStreamWriter(
                         new FileOutputStream(dest), "utf-8"
