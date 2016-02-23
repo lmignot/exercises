6ea50a66a1dee9256e8372706d506ec8a69c6b58
Laurent Mignot <laurent@mignot.eu>
2016-02-23 22:26:12 +0000

    fixed recursion tests from day 14 - binary search is for sorted lists doh!

--- day14/test/RecursionTests.java
+++ day14/test/RecursionTests.java
@@ -1,7 +1,7 @@
 import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,37 +10,98 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class RecursionTests {
 
-    @Test
-    public void binarySearchTestTenItems () {
-        Integer numItems = 10;
-        List<Integer> toSearch = new ArrayList<>(numItems);
+    private static final int TEN = 10;
+    private static final int FIFTY = 50;
+    private static final int ONE_HUNDRED = 100;
+    private static final int ONE_THOUSAND = 1000;
 
-        for(Integer i : toSearch) {
-            toSearch.add(i, (int) Math.abs(numItems * Math.random()));
+    private SearchUtils su = new SearchUtils();
+
+    @Test
+    public void mergeSortTestTenItems () {
+        List<Integer> toMerge = new ArrayList<>(TEN);
+
+        toMerge.add(3);
+        toMerge.add(9);
+        toMerge.add(4);
+        toMerge.add(8);
+        toMerge.add(2);
+        toMerge.add(1);
+        toMerge.add(0);
+        toMerge.add(5);
+        toMerge.add(6);
+        toMerge.add(7);
+
+        List<Integer> sorted = su.mergeSort(toMerge);
+
+        System.out.println(sorted);
+    }
+
+    @Test
+    public void mergeSortTestOneThousandItems () {
+        List<Integer> toMerge = new ArrayList<>(ONE_THOUSAND);
+
+        for(int i = 0; i < ONE_THOUSAND; i++) {
+            toMerge.add(i, (int) Math.abs(ONE_THOUSAND * Math.random()));
         }
 
-        boolean found = SearchUtils.search(toSearch, 6, new ConcurrentHashMap<>());
+        List<Integer> sorted = su.mergeSort(toMerge);
 
-        assertThat(found).isTrue();
+        System.out.println(sorted);
+    }
+
+    @Test
+    public void binarySearchTestTenItems () {
+        List<Integer> toSearch = new ArrayList<>(TEN);
+
+        toSearch.add(3);
+        toSearch.add(4);
+        toSearch.add(5);
+        toSearch.add(6);
+        toSearch.add(11);
+        toSearch.add(22);
+        toSearch.add(27);
+        toSearch.add(32);
+        toSearch.add(98);
+        toSearch.add(234);
+
+        assertThat(su.binarySearch(toSearch, 6)).isTrue();
+        assertThat(su.binarySearch(toSearch, 231)).isFalse();
+        assertThat(su.binarySearch(toSearch, 22)).isTrue();
+        assertThat(su.binarySearch(toSearch, 190)).isFalse();
+        assertThat(su.binarySearch(toSearch, 234)).isTrue();
+        assertThat(su.binarySearch(toSearch, 1)).isFalse();
     }
 
     @Test
     public void binarySearchTestOneHundredItems () {
-        Integer numItems = 100;
-        List<Integer> toSearch = new ArrayList<>(numItems);
+        List<Integer> toSearch = new ArrayList<>(ONE_HUNDRED);
 
-        for(Integer i : toSearch) {
-            toSearch.add(i, (int) Math.abs(numItems * Math.random()));
+        for(int i = 0; i < ONE_HUNDRED; i++) {
+            toSearch.add(i, i);
         }
+
+        assertThat(su.binarySearch(toSearch, 97)).isTrue();
+        assertThat(su.binarySearch(toSearch, 32)).isTrue();
+        assertThat(su.binarySearch(toSearch, 27)).isTrue();
+        assertThat(su.binarySearch(toSearch, 2)).isTrue();
+        assertThat(su.binarySearch(toSearch, 121)).isFalse();
+        assertThat(su.binarySearch(toSearch, 198)).isFalse();
     }
 
     @Test
     public void binarySearchTestOneThousandItems () {
-        Integer numItems = 1000;
-        List<Integer> toSearch = new ArrayList<>(numItems);
+        List<Integer> toSearch = new ArrayList<>(ONE_THOUSAND);
 
-        for(Integer i : toSearch) {
-            toSearch.add(i, (int) Math.abs(numItems * Math.random()));
+        for(int i = 0; i < ONE_THOUSAND; i++) {
+            toSearch.add(i, i);
         }
+
+        assertThat(su.binarySearch(toSearch, 970)).isTrue();
+        assertThat(su.binarySearch(toSearch, 322)).isTrue();
+        assertThat(su.binarySearch(toSearch, 275)).isTrue();
+        assertThat(su.binarySearch(toSearch, 189)).isTrue();
+        assertThat(su.binarySearch(toSearch, 1210)).isFalse();
+        assertThat(su.binarySearch(toSearch, 1980)).isFalse();
     }
 }
