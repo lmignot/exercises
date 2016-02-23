aeb2b18cf70f28a10ca9a726622d26422f05b697
Laurent Mignot <laurent@mignot.eu>
2016-02-23 22:27:17 +0000

    changed SearchUtils to not be static implemented mergeSort that accepts generic Lists

--- day14/src/SearchUtils.java
+++ day14/src/SearchUtils.java
@@ -1,35 +1,71 @@
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 /**
- * Binary search using recursion
+ * Search Utilities using recursion
  */
-public abstract class SearchUtils {
+public class SearchUtils <E extends Comparable<E>> {
 
-    public static boolean search (List<Integer> l, Integer query, Map<Integer, Boolean> cache) {
-        if (l == null || l.isEmpty()) {
+    public boolean binarySearch(List<Integer> l, int query) {
+        if (l.size() == 0) {
             return false;
         } else {
-            if (cache != null && cache.containsKey(query)) {
-                return true;
-            }
-
-            boolean found;
-            Integer half = l.size() / 2;
-            Integer middle = l.get(half);
-
-            if (middle.equals(query)) {
-                found = true;
-            } else {
-                found = (query < middle) ?
-                        search(l.subList(0, half), query, cache) :
-                        search(l.subList(half, l.size()), query, cache);
-            }
-            if (cache != null) {
-                cache.put(query, found);
-            }
-            return found;
+            return binarySearch(l, query, 0, l.size() - 1);
         }
     }
 
+    private boolean binarySearch(List<Integer> list, int query, int low, int high) {
+        if (low > high) { return false; }
+        int half = (low + high) / 2;
+        if (list.get(half) == query) { return true; }
+        else if (list.get(half) < query) {
+            return binarySearch(list, query, half + 1, high);
+        } else {
+            return binarySearch(list, query, low, half - 1);
+        }
+    }
+
+    public List<E> mergeSort (List<E> toSort) {
+        if (toSort.size() <= 1) {
+            return toSort;
+        }
+
+        int half = toSort.size() / 2;
+
+        List<E> left = mergeSort(toSort.subList(0, half));
+        List<E> right = mergeSort(toSort.subList(half, toSort.size()));
+
+        return merge(mergeSort(left), mergeSort(right));
+    }
+
+    private List<E> merge (List<E> a, List<E> b) {
+        List<E> result = new ArrayList<>();
+
+        int aSize = a.size(), bSize = b.size(), i = 0, j = 0;
+
+        // compare the lists and add the smaller item to the result
+        while (i < aSize && j < bSize) {
+            if (a.get(i).compareTo(b.get(j)) < 0) {
+                result.add(a.get(i));
+                i++;
+            } else {
+                result.add(b.get(j));
+                j++;
+            }
+        }
+
+        // list a has been completely checked, add remainder of list b to result
+        if (i == aSize) {
+            for (int k = j; k < bSize; k++) {
+                result.add(b.get(k));
+            }
+        } else if (j == bSize) {
+            // list b has been checked so add any remaining items in list a to result
+            for (int l = i; l < aSize; l++) {
+                result.add(a.get(l));
+            }
+        }
+
+        return result;
+    }
 }
