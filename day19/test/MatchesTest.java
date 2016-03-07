655b4940f31ea45492d16d2c5da277b66da09b0b
Laurent Mignot <laurent@mignot.eu>
2016-03-07 14:07:38 +0000

    tests for day 19 exercise 8

--- day19/test/MatchesTest.java
+++ day19/test/MatchesTest.java
@@ -27,31 +27,49 @@ public class MatchesTest {
     @Test
     public void matchesLengthLtFour () {
         List<String> shortWordsA = StringUtils.allMatches(stringListA, s -> s.length() < 4);
+        List<String> shortWordsAb = ElementUtils.allMatches(stringListA, s -> s.length() < 4);
         System.out.println("Words less than 4 characters in list a: " + shortWordsA);
+        System.out.println("Using generic version: " + shortWordsAb);
         List<String> shortWordsB = StringUtils.allMatches(stringListB, s -> s.length() < 4);
+        List<String> shortWordsBb = ElementUtils.allMatches(stringListB, s -> s.length() < 4);
         System.out.println("Words less than 4 characters in list b: " + shortWordsB);
+        System.out.println("Using generic version: " + shortWordsBb);
         List<String> shortWordsC = StringUtils.allMatches(stringListC, s -> s.length() < 4);
+        List<String> shortWordsCb = ElementUtils.allMatches(stringListC, s -> s.length() < 4);
         System.out.println("Words less than 4 characters in list c: " + shortWordsC);
+        System.out.println("Using generic version: " + shortWordsCb);
     }
 
     @Test
     public void containsN () {
         List<String> containsA = StringUtils.allMatches(stringListA, s -> s.contains("n"));
+        List<String> containsAb = ElementUtils.allMatches(stringListA, s -> s.contains("n"));
         System.out.println("List A, words containing 'n': " + containsA);
+        System.out.println("Using generic version: " + containsAb);
         List<String> containsB = StringUtils.allMatches(stringListB, s -> s.contains("n"));
+        List<String> containsBb = ElementUtils.allMatches(stringListA, s -> s.contains("n"));
         System.out.println("List B, words containing 'n': " + containsB);
+        System.out.println("Using generic version: " + containsBb);
         List<String> containsC = StringUtils.allMatches(stringListC, s -> s.contains("n"));
+        List<String> containsCb = ElementUtils.allMatches(stringListC, s -> s.contains("n"));
         System.out.println("List C, words containing 'n': " + containsC);
+        System.out.println("Using generic version: " + containsCb);
     }
 
     @Test
     public void evenLength () {
         List<String> evenA = StringUtils.allMatches(stringListA, s -> s.length() %2 == 0);
+        List<String> evenAb = ElementUtils.allMatches(stringListA, s -> s.length() %2 == 0);
         System.out.println("List A, even length words: " + evenA);
+        System.out.println("Using generic version: " + evenAb);
         List<String> evenB = StringUtils.allMatches(stringListB, s -> s.length() %2 == 0);
+        List<String> evenBb = ElementUtils.allMatches(stringListB, s -> s.length() %2 == 0);
         System.out.println("List B, even length words: " + evenB);
+        System.out.println("Using generic version: " + evenBb);
         List<String> evenC = StringUtils.allMatches(stringListC, s -> s.length() %2 == 0);
+        List<String> evenCb = ElementUtils.allMatches(stringListC, s -> s.length() %2 == 0);
         System.out.println("List C, even length words:" + evenC);
+        System.out.println("Using generic version: " + evenCb);
     }
 
 }
--- day19/test/TransformTest.java
+++ day19/test/TransformTest.java
@@ -13,7 +13,9 @@ public class TransformTest {
     private List<String> stringListB;
     private List<String> stringListC;
 
-    private String message = "Transformed list %s %s: %s";
+    private List<Integer> intListA;
+
+    private String message = "Transformed list %s %s: %s%n";
 
     @Before
     public void setUp () {
@@ -21,26 +23,58 @@ public class TransformTest {
                 "bear", "cat", "ox"};
         String[] b = {"restiveness", "jeopardy", "noon", "underquote", "huge", "tiny", "yam", "law", "subglobularity"};
         String[] c = {"cheesed", "unneedful", "undemonstrational", "amp", "zophar", "rad", "axe", "tui", "longueuil"};
+
+        Integer[] d = {0, 452, 22, 55, 9876, 2342, 12, 45, 67};
+
         stringListA = Arrays.asList(a);
         stringListB = Arrays.asList(b);
         stringListC = Arrays.asList(c);
+        intListA = Arrays.asList(d);
     }
 
     @Test
     public void replaceOWithZero () {
-        List<String> resultA = StringUtils.transformedList(stringListA, s -> s.replace("o", "0"));
+        List<String> resultA = StringUtils.transformList(stringListA, s -> s.replace("o", "0"));
         System.out.printf(message, "a,", "by replacing o with zero", resultA);
     }
 
     @Test
     public void replaceIWithEye () {
-        List<String> resultB = StringUtils.transformedList(stringListB, s -> s.replace("i", "EyE"));
+        List<String> resultB = ElementUtils.transformList(stringListB, s -> s.replace("i", "EyE"));
         System.out.printf(message, "b,", "by replacing i with EyE", resultB);
     }
 
     @Test
     public void uppercased () {
-        List<String> resultC = StringUtils.transformedList(stringListC, String::toUpperCase);
+        List<String> resultC = StringUtils.transformList(stringListC, String::toUpperCase);
         System.out.printf(message, "c,", "made everything uppercase", resultC);
+    }
+
+    @Test
+    public void addedTwo () {
+        System.out.println("Before transformation: " + Arrays.asList(intListA));
+        List<Integer> result = ElementUtils.transformList(intListA, i -> i + 2);
+        System.out.printf(message, "numbers,", "added 2", result);
+    }
+
+    @Test
+    public void divideByTwo () {
+        System.out.println("Before transformation: " + Arrays.asList(intListA));
+        List<Integer> result = ElementUtils.transformList(intListA, i -> i / 2);
+        System.out.printf(message, "numbers,", "divided by 2", result);
+    }
+
+    @Test
+    public void addedFifty () {
+        System.out.println("Before transformation: " + Arrays.asList(intListA));
+        List<Integer> result = ElementUtils.transformList(intListA, i -> i + 50);
+        System.out.printf(message, "numbers,", "added 50", result);
+    }
+
+    @Test
+    public void numbersToStrings () {
+        System.out.println("Before transformation: " + Arrays.asList(intListA));
+        List<String> result = ElementUtils.transformList(intListA, String::valueOf);
+        System.out.printf(message, "numbers,", "they're strings now, but it's hard to tell", result);
     }
 }
